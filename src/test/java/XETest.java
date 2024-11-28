9bc4543a00908873a709990459ba8c3fc3bcd504
Dimitris Tsiktsiris <dtsiktsiris@outlook.com>
2024-11-28 17:48:32 +0200

    add afterMethod driver.close()

--- src/test/java/XETest.java
+++ src/test/java/XETest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -30,7 +31,11 @@ public class XETest {
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.get("https://www.xe.gr/property/");
+    }
 
+    @AfterMethod
+    public void afterTest() {
+        driver.close();
     }
 
     @Test
